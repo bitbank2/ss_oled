946d61f6fc3ad29453e1571731e3df61ca7ffa2b
Larry Bank <bitbank@pobox.com>
2019-07-20 17:39:49 +0200

    Update simple_demo.ino
    updated for 2.0.0 API changes.

--- examples/simple_demo/simple_demo.ino
+++ examples/simple_demo/simple_demo.ino
@@ -11,8 +11,8 @@ rc = oledInit(OLED_128x64, 0, 0, -1, -1,400000L); // use standard I2C bus at 400
   if (rc != OLED_NOT_FOUND)
   {
     char *msgs[] = {"SSD1306 @ 0x3C", "SSD1306 @ 0x3D","SH1106 @ 0x3C","SH1106 @ 0x3D"};
-    oledFill(0);
-    oledWriteString(0,0,msgs[rc], FONT_NORMAL, 0);
+    oledFill(0, 1);
+    oledWriteString(0,0,0,msgs[rc], FONT_NORMAL, 0, 0);
     delay(2000);
   }
 }
@@ -23,16 +23,16 @@ int i, x, y;
 char szTemp[32];
 
   oledFill(0x0);
-  oledWriteString(16,0,(char *)"ss_oled Demo", FONT_NORMAL, 0);
-  oledWriteString(0,1,(char *)"Written by Larry Bank", FONT_SMALL, 1);
-  oledWriteString(0,3,(char *)"**Demo**", FONT_LARGE, 0);
+  oledWriteString(0,16,0,(char *)"ss_oled Demo", FONT_NORMAL, 0, 1);
+  oledWriteString(0,0,1,(char *)"Written by Larry Bank", FONT_SMALL, 1, 1);
+  oledWriteString(0,0,3,(char *)"**Demo**", FONT_LARGE, 0, 1);
   delay(2000);
-  oledFill(0);
+  oledFill(0, 1);
   for (i=0; i<3000; i++)
   {
     x = random(128);
     y = random(64);
-    oledSetPixel(x, y, 1);
+    oledSetPixel(x, y, 1, 1);
   }
   delay(2000);
 //
@@ -41,14 +41,14 @@ char szTemp[32];
 // To enable this functionality, define USE_BACKBUFFER at the beginning of ss_oled.cpp
 //
 #ifndef __AVR__
-  oledFill(0);
+  oledFill(0, 1);
   for (x=0; x<127; x+=2)
   {
-    oledDrawLine(x, 0, 127-x, 63);
+    oledDrawLine(x, 0, 127-x, 63, 1);
   }
   for (y=0; y<63; y+=2)
   {
-    oledDrawLine(127,y, 0,63-y);
+    oledDrawLine(127,y, 0,63-y, 1);
   }
   delay(2000);
 #endif // !__AVR__
