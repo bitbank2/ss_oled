881f7b00e2bfb176998b1cc03cad5ad869cb88fa
Larry Bank <bitbank@pobox.com>
2020-04-11 11:58:13 -0400

    fixed sample and font error

--- linux/sample.c
+++ linux/sample.c
@@ -14,7 +14,7 @@ unsigned char ucBackBuf[1024];
 int main(int argc, char *argv[])
 {
 int i, iChannel;
-int iOLEDAddr = 0x3c; // typical address; it can also be 0x3d
+int iOLEDAddr = -1; // typical address; it can also be 0x3d
 int iOLEDType = OLED_128x64; // Change this for your specific display
 int bFlip = 1, bInvert = 0;
 
@@ -23,16 +23,18 @@ int bFlip = 1, bInvert = 0;
 	while (i != 0 && iChannel < 2) // try I2C channel 0 through 2
 	{
 		iChannel++;
-		i=oledInit(&ssoled, iOLEDType, iOLEDAddr, bFlip, bInvert, 1, iChannel, 0x3c, -1, 400000);
+		i=oledInit(&ssoled, iOLEDType, iOLEDAddr, bFlip, bInvert, 1, iChannel, iOLEDAddr, -1, 400000);
 	}
-	if (i == 0)
+	if (i != OLED_NOT_FOUND)
 	{
+                char *msgs[] = {(char *)"SSD1306 @ 0x3C", (char *)"SSD1306 @ 0x3D",(char *)"SH1106 @ 0x3C",(char *)"SH1106 @ 0x3D"};
 		printf("Successfully opened I2C bus %d\n", iChannel);
                 oledSetBackBuffer(&ssoled, ucBackBuf);
 		oledFill(&ssoled, 0,1); // fill with black
-		oledWriteString(&ssoled, 0,0,0,"OLED 96 Library!",FONT_NORMAL,0,1);
-		oledWriteString(&ssoled, 0,3,1,"BIG!",FONT_LARGE,0,1);
-		oledWriteString(&ssoled, 0,0,1,"Small", FONT_SMALL,0,1);
+                oledWriteString(&ssoled, 0,0,0, msgs[i], FONT_NORMAL,0,1);
+		oledWriteString(&ssoled, 0,0,1,"SS_OLED Library!",FONT_NORMAL,0,1);
+		oledWriteString(&ssoled, 0,3,2,"BIG!",FONT_LARGE,0,1);
+		oledWriteString(&ssoled, 0,0,5,"Small", FONT_SMALL,0,1);
 		for (i=0; i<64; i++)
 		{
 			oledSetPixel(&ssoled, i, 16+i, 1, 1);
